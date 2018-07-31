246fd0dd54d22ce5adb0378a4578a6f959a808fd
Tucker Morgan <tucker_morgan@apple.com>
2018-07-30 21:24:14 -0700

    Change to scence file format

--- app/src/main.rs
+++ app/src/main.rs
@@ -24,8 +24,10 @@ fn render_chunk(i: usize, total_sections: usize, sender: Sender<ImageSection>){
     let _ = sender.send((i, image_0));
 }
 
-fn save_image(mut render_frames: Vec<ImageSection>, image_width: u32, image_height: u32) {
+fn save_image(mut render_frames: Vec<ImageSection>, image_width: u32, image_height: u32, mut final_image_path: &Path) {
+    
     render_frames.sort_unstable_by(|a, b| a.0.cmp(&b.0));
+
     let mut final_image_buffer : Vec<u8> = vec![];
     
     for ele in &render_frames {
@@ -34,41 +36,20 @@ fn save_image(mut render_frames: Vec<ImageSection>, image_width: u32, image_heig
     }
     let image_buf = ImageBuffer::<Rgb<u8>, Vec<u8>>::from_raw(image_width, image_height, final_image_buffer).unwrap();
     
-    let mut image_path_0 = Path::new("./out.png");
-    image_buf.save(&mut image_path_0).unwrap();
+    image_buf.save(&mut final_image_path).unwrap();
 }
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() == 1 {
-        println!("Please rerun with a number of threads stated as program arguments");
-        return;
-    }
-    
-    let try_parse = args[1].parse::<usize>();
-
-    let num_threads;
-
-    match try_parse {
-        Ok(val) => {
-            num_threads = val;
-        },
-        Err(e) => {
-            panic!("Error parsing thread count value: {}", e);
-        }
-    }
+fn render_image(scene_file_path: String, number_of_render_threads: usize, output_file: String) {
+    let now = Instant::now();
 
     let mut thread_handles = vec![];
     let (tx, rx) = channel();
 
-    let now = Instant::now();
-    println!("Number of threads {}", num_threads);
-
-    for i in 0..num_threads {
+    for i in 0..number_of_render_threads {
         let tx_clone = tx.clone();
         thread_handles.push(
             thread::Builder::new().name(i.to_string()).spawn(move || 
-                render_chunk(i, num_threads, tx_clone)
+                render_chunk(i, number_of_render_threads, tx_clone)
             ).unwrap()
         );
     }
@@ -80,11 +61,8 @@ fn main() {
     let mut finished_count = 0;
     let mut rendered_sections = vec![];
 
-    let scene_path = Path::new("scenes/test.json");
-    let scene_file =   File::open(scene_path).expect("File not found");
-    let scene: Scene = serde_json::from_reader(scene_file).unwrap();
 
-    while finished_count != num_threads {
+    while finished_count != number_of_render_threads {
         let result = rx.recv();
         match result {
             Ok(ele) => {
@@ -96,12 +74,39 @@ fn main() {
         }
         finished_count+=1;
     }
+    
     let elasped = now.elapsed();
     println!("{:?}", elasped);
 
-    save_image(rendered_sections, scene.width, scene.height);
+
+    let scene_path = Path::new(&scene_file_path);
+    let scene_file =   File::open(scene_path).expect("File not found");
+    let scene: Scene = serde_json::from_reader(scene_file).unwrap();
+
+    save_image(rendered_sections, scene.width, scene.height, Path::new(&output_file));
     let save_timer = now.elapsed() - elasped;
     println!("{:?}", save_timer);
 
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() == 1 {
+        println!("Please rerun with a number of threads stated as program arguments");
+        return;
+    }
+    
+    let try_parse = args[1].parse::<usize>();
+    let num_threads;
+
+    match try_parse {
+        Ok(val) => {
+            num_threads = val;
+        },
+        Err(e) => {
+            panic!("Error parsing thread count value: {}", e);
+        }
+    }
+    render_image(String::from("scenes/test.json"), num_threads, String::from("./out.png"));
     return;
 }
